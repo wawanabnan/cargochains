709965c4c0aa69e40dddc429e17f729e476097bc
bin abnan <wawanov@gmail.com>
2026-02-27 13:49:35 +0700

    update update

--- work_orders/views/service_orders.py
+++ work_orders/views/service_orders.py
@@ -604,9 +604,13 @@ class VendorBookingCreateView(LoginRequiredMixin, TemplateView):
 
     def post(self, request, *args, **kwargs):
         job_id = (request.POST.get("job_order") or "").strip()
+        #if not job_id.isdigit():
+        #    messages.error(request, "Job Order wajib dipilih.")
+        #    return redirect(f"{reverse('work_orders:service_order_create')}?job_order={job.id}")
+
         if not job_id.isdigit():
             messages.error(request, "Job Order wajib dipilih.")
-            return redirect(f"{reverse('work_orders:service_order_create')}?job_order={job.id}")
+            return redirect(f"{reverse('work_orders:service_order_create')}?job_order={job_id}")
 
 
         job = get_object_or_404(JobOrder, pk=int(job_id))
